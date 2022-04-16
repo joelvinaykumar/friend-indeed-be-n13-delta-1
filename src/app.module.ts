080949a0c49f14abae18f77aa46a7260888d9750
Joel Vinay Kumar <lavotivinay@gmail.com>
2022-04-16 12:33:08 +0530

    Hide razorpay keys

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { RazorpayModule } from 'nestjs-razorpay';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+
 import { ThrottlerBehindProxyGuard } from './common/guards/throttler-behind-proxy.guard';
 import { AppController } from './app.controller';
 import { TherapistModule } from './therapist/therapist.module';
@@ -8,13 +10,12 @@ import { FeedbackModule } from './feedback/feedback.module';
 import { CategoryModule } from './category/category.module';
 import { BookingModule } from './booking/booking.module';
 import { PatientModule } from './patient/patient.module';
-import { RazorpayModule } from 'nestjs-razorpay';
 import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     RazorpayModule.forRoot({
-      key_id: 'rzp_test_F0sBYBI61kGSeC',
-      key_secret: 'HihQcOHNIr8KJzjOd4RJV8S7',
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
     }),
     ThrottlerModule.forRoot({
       ttl: 60,
